49bf573eb4415ff2a6c827b08863904ff3183216
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-31 16:44:28 +0430

    the first setup

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+  return (
+    <>
+        <div>Number of cakes</div>
+        <button>buy a cake</button>
+    </>
+  )
+}
+
+export default CakeContainer
