a394f6c08e4dc2a2e687032af9c7c9ce1b656ce7
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-31 18:16:31 +0430

    using redux hooks to get the state date and dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import CakeContainer from './components/CakeContainer'
 import { Provider } from 'react-redux'
 import  store  from './redux/cake/cakeStore'
+import HooksCakeContainer from './components/HooksCakeContainer'
 const App = () => {
   return (
     <Provider store={store}>
       <div className='App'>
         <CakeContainer/>
+        <HooksCakeContainer/>
       </div>
     </Provider>
   )
